835240ebf3d7b0246703d73da88e8016802e6493
tonywang10101 <tony.wang.10101@gmail.com>
2024-01-23 09:17:08 +0000

    fix: fix output format

--- llama2_7b_chat/1/model.py
+++ llama2_7b_chat/1/model.py
@@ -353,15 +353,13 @@ class Llama2Chat:
 
         print(f"Inference time cost {time.time()-t0}s")
 
-        # text_outputs = [
-        #     seq["generated_text"].encode("utf-8")
-        #     for seq in sequences
-        # ]
-        # task_output = serialize_byte_tensor(np.asarray(text_outputs))
-        task_outputs = [StandardTaskIO.parse_task_text_generation_output(sequences)]
+        text_outputs = [seq["generated_text"].encode("utf-8") for seq in sequences]
+        task_output = serialize_byte_tensor(np.asarray(text_outputs))
+        # task_output = StandardTaskIO.parse_task_text_generation_output(sequences)
 
         print("Output:")
-        print(task_outputs)
+        print(task_output)
+        print(type(task_output))
 
         return construct_infer_response(
             req=req,
@@ -372,7 +370,7 @@ class Llama2Chat:
                     shape=[-1, -1],
                 )
             ],
-            raw_outputs=task_outputs,
+            raw_outputs=[task_output],
         )
 
 
